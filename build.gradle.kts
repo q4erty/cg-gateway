8db9ce886680ad0c4248d32d118f7061925cf547
NBayjanov <NBayjanov@beeline.kz>
2026-06-18 15:56:43 +0500

    feat: add code coverage checks and CI pipeline configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.3.21"
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 group = "com.cloudgaming"
@@ -46,6 +47,16 @@ dependencyManagement {
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+kover {
+    reports {
+        verify {
+            rule {
+                minBound(70)
+            }
+        }
     }
 }
 
